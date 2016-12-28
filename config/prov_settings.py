22d37d2467de5d75a606534c9e53e2780448a402
Joway <joway@joway.wang>
2016-12-28 19:57:23 +0800

    Update prov_settings.py

--- config/prov_settings.py
+++ config/prov_settings.py
@@ -2,12 +2,10 @@ import os
 
 ENV = {
     'DEBUG': 'False',
-    'SOCIAL_AUTH_XIAOMA_CLIENT_ID': 'xiaoma',
-    'SOCIAL_AUTH_XIAOMA_SECRET': '201d500504b510f9e70bce56db10d257',
     'SECRET_KEY': '+p$6nzend1smn!*etr7k$9*g#$nj!lw#=u2e@ga82sra#nral3',
-    'MYSQL_HOST': 'xiaoma.joway.wang',
+    'MYSQL_HOST': 'xxx',
     'MYSQL_USERNAME': 'root',
-    'MYSQL_PASSWORD': 'FUCKaliyunMYSQL',
+    'MYSQL_PASSWORD': 'xxx',
     'MYSQL_INSTANCE_NAME': 'xiaoma',
     'MYSQL_PORT': '33060',
 }
